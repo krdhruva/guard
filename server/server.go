f4a783e5bc05a0a546b0276db61ba2d59f2d71cf
Krupesh Dhruva <krdhruva@microsoft.com>
2020-03-17 13:33:52 +0530

    fixing compilation error

--- server/server.go
+++ server/server.go
@@ -154,7 +154,7 @@ func (s Server) ListenAndServe() {
 		}
 	}))
 
-	if len(s.RecommendedOptions.AuthzProvider.providers) > 0 {
+	if len(s.RecommendedOptions.AuthzProvider.Providers) > 0 {
 		m.Post("/subjectaccessreviews", http.HandlerFunc(s.Authzhandler))
 	}
 	srv := &http.Server{
