484e9ef87a5e09a6d8b908254ebeeef3af1181dc
Ubuntu <azureuser@kube-master.axjqua5aeqjujnl4vflawpxarf.bx.internal.cloudapp.net>
2021-08-25 10:04:03 +0000

    dogfood

--- authz/providers/azure/rbac/rbac.go
+++ authz/providers/azure/rbac/rbac.go
@@ -53,6 +53,7 @@ const (
 type AuthzInfo struct {
 	AADEndpoint string
 	ARMEndPoint string
+        ARMAudience string
 }
 
 type void struct{}
@@ -147,7 +148,7 @@ func New(opts authzOpts.Options, authopts auth.Options, authzInfo *AuthzInfo) (*
 	case authzOpts.ARCAuthzMode:
 		tokenProvider = graph.NewClientCredentialTokenProvider(authopts.ClientID, authopts.ClientSecret,
 			fmt.Sprintf("%s%s/oauth2/v2.0/token", authzInfo.AADEndpoint, authopts.TenantID),
-			fmt.Sprintf("%s.default", authzInfo.ARMEndPoint))
+			fmt.Sprintf("%s.default", authzInfo.ARMAudience))
 	case authzOpts.AKSAuthzMode:
 		tokenProvider = graph.NewAKSTokenProvider(opts.AKSAuthzTokenURL, authopts.TenantID)
 	}
